4df96bcfd7433370c78045a6f996fdee34b76ba6
Your NameMariabella <ramosmariabella@gmail.com>
2022-11-08 20:35:41 -0800

    Completed calculator functions

--- arithmetic.py
+++ arithmetic.py
@@ -1,35 +1,35 @@
 """Functions for common math operations."""
 
-
 def add(num1, num2):
     """Return the sum of num1 and num2."""
-
-    return 10
-
+    return(num1+num2)
+   #return 10
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
-
+    return(num1-num2)
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
-
+    return(num1*num2)
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
-
+    division_operation= num1/num2
+    return(float(division_operation))
 
 def square(num1):
     """Return the square of num1."""
-
+    return(num1*num1)
 
 def cube(num1):
     """Return the cube of num1."""
-
+    return(num1*num1*num1)
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    return( pow(num1,num2))
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    return num1%num2
